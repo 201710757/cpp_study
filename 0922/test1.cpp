81382ccd662eb2655feb7d1ac198ab5f405734ac
201710757 <neti69@naver.com>
2022-09-22 17:31:25 +0900

    0922 - 2

--- 0922/test1.cpp
+++ 0922/test1.cpp
@@ -1,48 +1,55 @@
 #include<iostream>
 #include<vector>
+#include<deque>
+
 using namespace std;
 
 int N, K, X, M;
-int board[300001];
+int board[300010];
+vector<int>v_l[300010];
+deque<int> que;
 
 
-vector<int> res;
 int main(void)
 {
+    
     cin >> N >> M >> K >> X;
 
     for(int i=0;i<M;i++)
     {
         int s, e;
         cin >> s >> e;
-
-        if(s == X)
-        {
-            board[e] = 1;
-        }
-        else
-        {
-            if(board[s] != 0)
-            {
-                if(board[s] + 1 < board[e]) board[e] = board[s] + 1;
-                else if (board[e] == 0) board[e] = board[s] + 1;
-            }
-            else
-            {
-                board[e] = board[s] + 1;
-            }
-        }
-
+        v_l[s].push_back(e);
     }
 
+    que.push_back(X);
+    while(!que.empty())
+    {
+        int pos = que.front();
+        que.pop_front();
+
+        for(auto p : v_l[pos])
+        {
+            if(board[p] == 0)
+            {
+                board[p] = board[pos] + 1;
+            }
+            
+            que.push_back(p);
+        }
+    }
+
+
+    bool _flag = true;
     for(int i=1;i<=N;i++)
     {
-        if(board[i] == K) res.push_back(i);
+        if(board[i] == K && i != X) 
+        {
+            _flag = false;
+            cout<<i<<endl;
+        }
     }
-    if(res.size() == 0) cout<<-1<<endl;
-    else
-    {
-        for(int i=0;i<res.size();i++) cout<<res[i]<<endl;
-    }
+    if(_flag) cout<<-1<<endl;
+    
     return 0;
 }
